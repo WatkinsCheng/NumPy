fcd9f0ba5273f4d77241b6873fd223b0a620dd21
qiwenzheng <875826697@qq.com>
2018-12-03 22:47:49 +0800

    Initial commit

---
+++ Study.py
@@ -0,0 +1,8 @@
+import numpy as np
+data = [[1, 2, 3, 4], [4, 3, 2, 1], [3, 3, 2, 7]]
+test = np.array(data, dtype=np.float)
+print(test)
+print(test.dtype)
+print(test.ndim)
+print(test.shape)
+print(type(test))
